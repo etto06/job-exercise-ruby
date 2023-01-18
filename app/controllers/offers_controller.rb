acb92e511759a11cd41d125b45e234baf40d94a6
Ettore Canever <etto06etto@gmail.com>
2023-01-18 08:51:32 +0000

    finish

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -65,6 +65,6 @@ class OffersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def offer_params
-      params.require(:offer).permit(:rif, :titolo, :descrizione, :luogo)
+      params.require(:offer).permit(:rif, :titolo, :descrizione, :luogo, :foto)
     end
 end
